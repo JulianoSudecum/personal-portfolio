73aa7c87d18ece4eaaaa4e2cb58764c8cfb91908
Juliano <julianosudecum525@gmail.com>
2023-07-31 10:49:32 -0300

    fix: att port

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -10,12 +10,12 @@ function App() {
       <div className='first_div'>
         <p className='main_text'>
         <span className='my_name'>OI, EU SOU O JULIANO <MdWavingHand id='handle_icon' /></span><br/>
-        <span className='my_stack'>REACT
+        <span className='my_stack'>Web
         <a id='linkedin' target="_blank" href='https://www.linkedin.com/in/julianosudecum/' className='button_social'> <AiFillLinkedin size={32} /></a>
         <a target="_blank" href='https://github.com/JulianoSudecum' className='button_social'> <AiFillGithub size={32} /></a>
         <br/>
         DEVELOPER</span><br/>
-        <span className='my_desc'>Tenho 19 anos,<br/> sou atualmente desenvolvedor front-end<br/> com 6 meses de experiencia trabalhando em projetos.</span>
+        <span className='my_desc'>Tenho 19 anos,<br/> sou atualmente desenvolvedor <span id='full_stack_id'>Full-Stack</span><br/> com 1 ano de experiencia trabalhando em projetos.</span>
         </p>
       </div>
       <div className='slide_project'>
